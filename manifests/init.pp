f080249bd904368b3194c632784137091d0c22fc
root <root@puppet-server.cdl.local>
2017-10-03 00:27:12 +0000

    Add module skeleton

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,21 @@
 #
 # Copyright 2017 Your name here, unless otherwise noted.
 #
-class nginx {
-
-
+class nginx(
+	String $ensure 			= $::nginx::params::ensure,
+	String $package_name		= $::nginx::params::package_name,
+	String $config_dir		= $::nginx::params::config_dir,
+	String $config_ensure		= $::nginx::params::config_ensure,
+	String $config_mode		= $::nginx::params::config_mode,
+	String $config_owner		= $::nginx::params::config_owner,
+	String $config_group		= $::nginx::params::config_group,
+	String $config_confd		= $::nginx::params::config_confd,
+	String $config_log_dir		= $::nginx::params::config_log_dir,
+	String $config_pid_file		= $::nginx::params::config_pid_file,
+	Optional[String] $config_vdir_enable = $::nginx::params::config_vdir_enable
+) inherits ::nginx::params
+{
+	class { '::nginx::install': }
+	-> class { '::nginx::config': }
+	-> class { '::nginx::service': }
 }
